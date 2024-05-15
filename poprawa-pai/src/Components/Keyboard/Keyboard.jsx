7c8fa506cbbc866bfef3bd522fc925b98332fba4
filip k <filip2006kujawa@gmail.com>
2024-05-15 09:31:53 +0200

    a

---
+++ poprawa-pai/src/Components/Keyboard/Keyboard.jsx
@@ -0,0 +1,6 @@
+import { useEffect, useState } from "react";
+
+const Keyboard = () => {
+  const [selectedLetters, setSelectedLetters] = useState([]);
+  //   #uzywajac useEffect zrobic dodoaw do setSelectedLetters
+};
